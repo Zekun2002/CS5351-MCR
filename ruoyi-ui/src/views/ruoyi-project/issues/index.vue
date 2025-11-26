f742561a797bd869ae4aa9045b4d678cca2d2ea3
xrh <x15119999577@163.com>
2025-11-26 21:52:05 +0800

    前端

--- ruoyi-ui/src/views/ruoyi-project/issues/index.vue
+++ ruoyi-ui/src/views/ruoyi-project/issues/index.vue
@@ -201,7 +201,7 @@
           <el-input v-model="form.createdBy" placeholder="请输入创建问题的用户ID" />
         </el-form-item> -->
         <el-form-item label="创建问题的用户ID" prop="userId">
-          <el-select v-model="form.userId" placeholder="请输入创建问题的用户ID" >
+          <el-select v-model="form.createdBy" placeholder="请输入创建问题的用户ID" >
             <el-option
               v-for="item in userIds"
               :key="item.userId"
